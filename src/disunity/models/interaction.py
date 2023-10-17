c943332d284abe187c166cc3cb6ab5690beac207
milan_25 <milan.abraham2@seznam.cz>
2023-10-17 16:42:44 +0200

    Fixed breaking changes by Discord

--- src/disunity/models/interaction.py
+++ src/disunity/models/interaction.py
@@ -2,12 +2,11 @@ from ..errors import InvalidMethodUse
 from .user import User
 from .member import Member
 
+
 class Interaction:
     def __init__(self, received):
         self.raw: dict = received
-        self.app_permissions: None | int = received.get("app_permissions", None)
-        if self.app_permissions is not None:
-            self.app_permissions = int(self.app_permissions)
+        self.app_permissions: int = int(received.get("app_permissions", 0))
 
         self.channel_id: int = int(received["channel_id"])
         self.id: int = int(received["id"])
@@ -24,27 +23,31 @@ class Interaction:
         self.command_name: str = self.data.get("name", None)
         self.modal_values: None | list[dict] = None
 
-        if 'member' in received: # Not a DM command
+        if "member" in received:  # Not a DM command
             self.invoked_by: User = User(received["member"]["user"])
             self.member = Member(received["member"])
         else:
             self.invoked_by: User = User(received["user"])
 
-        if self.interaction_type == 3: # Component
-            self.invoked_by: User = User(received["message"]["interaction"]["user"])
-            if 'member' in received:
+        if self.interaction_type == 3:  # Component
+            if "interaction" in received["message"]:
+                self.invoked_by: User = User(received["message"]["interaction"]["user"])
+            else:
+                self.invoked_by: User = None
+            if "member" in received:
                 self.used_by: None | User = User(received["member"]["user"])
             else:
                 self.used_by: None | User = User(received["user"])
 
-        elif self.interaction_type == 5: # modal submit
-            self.modal_values: None | list[dict] = self.data["components"][0]["components"]   
-
+        elif self.interaction_type == 5:  # modal submit
+            self.modal_values: None | list[dict] = self.data["components"][0][
+                "components"
+            ]
 
     def check_user(self) -> bool:
         """
-            For components, checks if the component user and
-            the interaction author are the same.
+        For components, checks if the component user and
+        the interaction author are the same.
         """
 
         if self.interaction_type != 3:
--- src/disunity/models/member.py
+++ src/disunity/models/member.py
@@ -1,15 +1,16 @@
 from .user import User
 from .. import utils
 
+
 class Member:
     def __init__(self, received: dict):
         self.raw: dict = received
         self.deaf: bool = received["deaf"]
-        self.is_pending: bool = received["is_pending"]
+        self.pending: bool = received.get("pending", False)
         self.joined_at = received["joined_at"]
         self.mute: bool = received["mute"]
         self.nick: str = received["nick"]
-        self.permissions: int = int(received["permissions"])
+        self.permissions: int = int(received.get("permissions", 0))
         self.roles: list[int] = [int(role) for role in received["roles"]]
         self.user: User = User(received["user"])
         self.flags: int = int(received["flags"])
@@ -18,4 +19,4 @@ class Member:
     def server_avatar_url(self) -> None | str:
         if self.raw["avatar"] != "None":
             return utils.return_avatar_as_cdn(self.raw["avatar"], self.user.id)
-        return None 
+        return None
